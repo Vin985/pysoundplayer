d928615704f237dd2fa0901fecb7255d0714e8a6
Vin985 <sylvain.christin@gmail.com>
2020-02-07 16:48:59 -0400

    Settings loading simplification

--- src/pysoundplayer/gui/settings.py
+++ src/pysoundplayer/gui/settings.py
@@ -17,6 +17,16 @@ class SoundPlayerSettings(QSettings):
                                 self.GROUP_IMAGE: ImageGenerator.DEFAULT_OPTIONS}
         self.options = {self.GROUP_SPECTROGRAM: self.spectrogram_settings(
         ), self.GROUP_IMAGE: self.image_settings()}
+        self.all_settings()
+        self.save_group(self.GROUP_SPECTROGRAM, self.options["spectrogram"])
+        self.save_group(self.GROUP_IMAGE, self.options[self.GROUP_IMAGE])
+
+    def all_settings(self):
+        print("loading all keys")
+        res = {}
+        for key in self.allKeys():
+            res[key] = self.value(key)
+        print(res)
 
     @property
     def spectrogram_options(self):
@@ -81,18 +91,20 @@ class SoundPlayerSettings(QSettings):
                 "No entries found for selected context, using default group")
             self.endGroup()
             self.beginGroup(group)
-        res["window"] = self.get_spec_setting("window")
-        res["n_fft"] = self.get_spec_setting("n_fft")
-        res["pcen"] = self.get_spec_setting("pcen")
-        res["to_db"] = self.get_spec_setting("to_db")
-        res["normalize"] = self.get_spec_setting("normalize")
-        res["hop_length"] = self.get_spec_setting("hop_length")
-        res["scale"] = self.get_spec_setting("scale")
-        res["remove_noise"] = self.get_spec_setting("remove_noise")
-        res["nr_hist_rel_size"] = self.get_spec_setting("nr_hist_rel_size")
-        res["nr_N"] = self.get_spec_setting("nr_N")
-        res["nr_window_smoothing"] = self.get_spec_setting(
-            "nr_window_smoothing")
+        for key in self.childKeys():
+            res[key] = self.get_spec_setting(key)
+        # res["window"] = self.get_spec_setting("window")
+        # res["n_fft"] = self.get_spec_setting("n_fft")
+        # res["pcen"] = self.get_spec_setting("pcen")
+        # res["to_db"] = self.get_spec_setting("to_db")
+        # res["normalize"] = self.get_spec_setting("normalize")
+        # res["hop_length"] = self.get_spec_setting("hop_length")
+        # res["scale"] = self.get_spec_setting("scale")
+        # res["remove_noise"] = self.get_spec_setting("remove_noise")
+        # res["nr_hist_rel_size"] = self.get_spec_setting("nr_hist_rel_size")
+        # res["nr_N"] = self.get_spec_setting("nr_N")
+        # res["nr_window_smoothing"] = self.get_spec_setting(
+        #     "nr_window_smoothing")
         self.endGroup()
         return res
 
@@ -101,15 +113,16 @@ class SoundPlayerSettings(QSettings):
     def image_settings(self):
         res = {}
         self.beginGroup(self.GROUP_IMAGE)
-        res["color_masks_str"] = self.get_image_setting(
-            "color_masks_str")
-        res["contrast"] = self.get_image_setting("contrast")
-        res["invert_colors"] = self.get_image_setting("invert_colors")
-        res["composite_ffts"] = self.get_image_setting("composite_ffts")
-        res["height"] = self.get_image_setting("height")
-        res["pixels_in_sec"] = self.get_image_setting("pixels_in_sec")
-        res["color_map"] = self.get_image_setting("color_map")
+        # res["color_masks_str"] = self.get_image_setting(
+        #     "color_masks_str")
+        # res["contrast"] = self.get_image_setting("contrast")
+        # res["invert_colors"] = self.get_image_setting("invert_colors")
+        # res["composite_ffts"] = self.get_image_setting("composite_ffts")
+        # res["height"] = self.get_image_setting("height")
+        # res["pixels_in_sec"] = self.get_image_setting("pixels_in_sec")
+        # res["color_map"] = self.get_image_setting("color_map")
+        for key in self.childKeys():
+                res[key] = self.get_image_setting(key)
         # res["composite_ffts"] = [int(i) for i in composite_ffts]
         self.endGroup()
-        print(res)
         return res
